8e593afa0b7add3ece60a62b18cecd3d0d3f7722
kaustabh-das <kaustabhkumar1997@gmail.com>
2021-11-19 11:40:52 -0500

    first commit on 19/11/21

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Body from "../components/Body/Body";
+import Hero from "../components/Hero/Hero";
+
+const home = () => {
+  return (
+    <div>
+      <Hero />
+      <Body />
+    </div>
+  );
+};
+
+export default home;
